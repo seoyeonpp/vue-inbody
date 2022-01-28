0529bc99089cc9e6757e1e86248d16f582b84aad
서도하 Doha Seo <seodoha@reflexion.co.kr>
2022-01-28 12:00:14 +0900

    도하- 중간커밋

--- vue-inbody/src/views/login/Login.vue
+++ vue-inbody/src/views/login/Login.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="full-wrap">
+        <div class="inner-box">
+            <div class="input-box">
+                <v-text-field
+                    label="Login"
+                    :rules="rules"
+                    hide-details="auto"
+                    ></v-text-field>
+            </div>
+            <div class="input-box">
+                <v-text-field
+                    label="Password"
+                    :rules="rules"
+                    hide-details="auto"
+                    ></v-text-field>
+            </div>
+            <div class="button-box">
+                <router-link to="/">회원가입</router-link>
+                <router-link to="/">로그인</router-link>
+            </div>
 
-        <div class="input-box">
-            <v-text-field
-                label="Login"
-                :rules="rules"
-                hide-details="auto"
-                ></v-text-field>
-        </div>
-        <div class="input-box">
-            <v-text-field
-                label="Login"
-                :rules="rules"
-                hide-details="auto"
-                ></v-text-field>
         </div>
     </div>
 </template>
