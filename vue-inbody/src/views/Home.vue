9b4a145debbc2f4b6de758d0f685db8a8e35fcf1
박서연 <seoyeonpub@gmail.com>
2023-01-16 23:12:36 +0900

    Home.vue 임시변경

--- vue-inbody/src/views/Home.vue
+++ vue-inbody/src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-  <hello-world />
+  <!-- <hello-world /> -->
+  <div>
+    <router-link to="/uicomponents">uicomponents</router-link>
+    <router-link to="/chartsample">chartsample</router-link>
+    <router-link to="/login">로그인</router-link>
+    <router-link to="/dashboard">대시보드</router-link>
+    <router-link to="/boardList">boardList</router-link>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+// import HelloWorld from "../components/HelloWorld";
 
 export default {
   name: "Home",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+  a{display: block;}
+  a + a{margin-top: 10px;}
+</style>
